b60e66b1769a86e7d197e2dc4cf06250419be21e
proelbtn <contact@proelbtn.com>
2021-08-09 19:51:59 +0900

    ベンチマーク用のコードを追加した
    Connection pool無し: mean = 80.45896151171875, stddev = 9.491444921084323
    Connection pool有り: mean = 33.84948690234376, stddev = 3.5228100383565466

---
+++ scripts/benchmark_auth_self.py
@@ -0,0 +1,30 @@
+from ictsc2021 import Rikka
+
+import time
+from math import pow, sqrt
+
+
+def main():
+    rikka = Rikka(baseurl="http://localhost:8080/api")
+
+    print(f"\x1b[33m\n*** signin\x1b[0m")
+    rikka.signin("ictsc", "")
+
+    print(f"\x1b[33m\n*** get self\x1b[0m")
+    results = []
+    for i in range(256):
+        start = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+        rikka.self()
+        end = time.clock_gettime_ns(time.CLOCK_MONOTONIC)
+
+        req_ms = (end - start) / 1000000
+        results.append(req_ms)
+        print(f"{i} - {req_ms}")
+
+    mean = sum(results) / len(results)
+    stddev = sqrt(sum([pow(res - mean, 2) for res in results]) / (len(results) - 1))
+
+    print(f"mean = {mean}, stddev = {stddev}")
+
+
+main()
