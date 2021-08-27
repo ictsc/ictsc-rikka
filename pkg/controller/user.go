328758cf00d007a2231bf272286ddc0e9fc90ed0
proelbtn <contact@proelbtn.com>
2021-08-27 23:13:40 +0900

    ユーザ周りの制限を入力した

--- pkg/controller/user.go
+++ pkg/controller/user.go
@@ -17,8 +17,8 @@ func NewUserController(userService *service.UserService) *UserController {
 }
 
 type CreateUserRequest struct {
-	Name           string `json:"name" binding:"required,max=32,excludesall=\r\n"`
-	Password       string `json:"password" binding:"required,min=8,max=40,excludesall=\r\n"`
+	Name           string `json:"name" binding:"required,min=3,max=32,printascii"`
+	Password       string `json:"password" binding:"required,min=8,max=40,printascii"`
 	UserGroupID    string `json:"user_group_id" binding:"required,uuid"`
 	InvitationCode string `json:"invitation_code" binding:"required"`
 }
@@ -59,8 +59,8 @@ type UpdateUserRequest struct {
 	DisplayName      string `json:"display_name" binding:"required,max=32,excludesall=\r\n"`
 	TwitterID        string `json:"twitter_id" binding:"omitempty,max=15,printascii"`
 	GithubID         string `json:"github_id" binding:"omitempty,max=39,printascii"`
-	FacebookID       string `json:"facebook_id" binding:"omitempty,max=64,printascii"`//正確なmaxの値が不明
-	SelfIntroduction string `json:"self_introduction" binding:"max=500"`
+	FacebookID       string `json:"facebook_id" binding:"omitempty,max=64,printascii"` //正確なmaxの値が不明
+	SelfIntroduction string `json:"self_introduction" binding:"max=96"`
 }
 
 type UpdateUserResponse struct {
