0433a973535d92744b8ca6aebf98c9f77edb9435
proelbtn <contact@proelbtn.com>
2021-08-27 18:29:13 +0900

    add answer limit

--- pkg/service/answer.go
+++ pkg/service/answer.go
@@ -12,24 +12,25 @@ import (
 )
 
 type AnswerService struct {
+	answerLimit time.Duration
 	userRepo    repository.UserRepository
 	answerRepo  repository.AnswerRepository
 	problemRepo repository.ProblemRepository
 }
 
 type CreateAnswerRequest struct {
-	Point       uint
-	Body        string
-	UserGroupID uuid.UUID
-	ProblemID   uuid.UUID
+	UserGroup *entity.UserGroup
+	Body      string
+	ProblemID uuid.UUID
 }
 
 type UpdateAnswerRequest struct {
 	Point uint
 }
 
-func NewAnswerService(userRepo repository.UserRepository, answerRepo repository.AnswerRepository, problemRepo repository.ProblemRepository) *AnswerService {
+func NewAnswerService(answerLimit int, userRepo repository.UserRepository, answerRepo repository.AnswerRepository, problemRepo repository.ProblemRepository) *AnswerService {
 	return &AnswerService{
+		answerLimit: time.Duration(answerLimit) * time.Minute,
 		userRepo:    userRepo,
 		answerRepo:  answerRepo,
 		problemRepo: problemRepo,
@@ -38,23 +39,23 @@ func NewAnswerService(userRepo repository.UserRepository, answerRepo repository.
 
 func (s *AnswerService) Create(req *CreateAnswerRequest) (*entity.Answer, error) {
 	lastAnswered := time.Time{}
-	pastAnswers, err := s.answerRepo.FindByUserGroup(req.UserGroupID)
+	pastAnswers, err := s.answerRepo.FindByUserGroup(req.UserGroup.ID)
 	if err != nil {
 		return nil, err
 	}
 
 	for _, answer := range pastAnswers {
-		if answer.CreatedAt.After(lastAnswered) {
+		if answer.CreatedAt.After(lastAnswered) && answer.ProblemID == req.ProblemID {
 			lastAnswered = answer.CreatedAt
 		}
 	}
 
-	if !time.Now().After(lastAnswered.Add(1 * time.Minute)) {
+	if !time.Now().After(lastAnswered.Add(s.answerLimit)) {
 		return nil, e.NewForbiddenError("couldn't submit answer if you submit answer within last 20 minutes")
 	}
 
 	ans := &entity.Answer{
-		UserGroupID: req.UserGroupID,
+		UserGroupID: req.UserGroup.ID,
 		Point:       nil,
 		Body:        req.Body,
 		ProblemID:   req.ProblemID,
@@ -71,17 +72,40 @@ func (s *AnswerService) Create(req *CreateAnswerRequest) (*entity.Answer, error)
 	return s.answerRepo.Create(ans)
 }
 
-func (s *AnswerService) GetAll() ([]*entity.Answer, error) {
-	return s.answerRepo.GetAll()
-}
+func (s *AnswerService) FindByID(group *entity.UserGroup, id uuid.UUID) (*entity.Answer, error) {
+	ans, err := s.answerRepo.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
 
-func (s *AnswerService) FindByID(id uuid.UUID) (*entity.Answer, error) {
-	return s.answerRepo.FindByID(id)
+	if !group.IsFullAccess && !time.Now().After(ans.CreatedAt.Add(s.answerLimit)) {
+		ans.Point = nil
+	}
+
+	return ans, nil
 }
 
 // userGroupID is optional
-func (s *AnswerService) FindByProblem(probid uuid.UUID, userGroupID *uuid.UUID) ([]*entity.Answer, error) {
-	return s.answerRepo.FindByProblem(probid, userGroupID)
+func (s *AnswerService) FindByProblem(group *entity.UserGroup, probid uuid.UUID, userGroupID *uuid.UUID) ([]*entity.Answer, error) {
+	if !group.IsFullAccess && userGroupID != nil && group.ID != *userGroupID {
+		return nil, e.NewForbiddenError("you cannot fetch other group's answers")
+	}
+
+	answers, err := s.answerRepo.FindByProblem(probid, userGroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	if !group.IsFullAccess {
+		now := time.Now()
+		for _, ans := range answers {
+			if !now.After(ans.CreatedAt.Add(s.answerLimit)) {
+				ans.Point = nil
+			}
+		}
+	}
+
+	return answers, nil
 }
 
 func (s *AnswerService) FindByUserGroup(id uuid.UUID) ([]*entity.Answer, error) {
@@ -89,8 +113,26 @@ func (s *AnswerService) FindByUserGroup(id uuid.UUID) ([]*entity.Answer, error) 
 }
 
 // userGroupID is require
-func (s *AnswerService) FindByProblemAndUserGroup(probid uuid.UUID, userGroupID uuid.UUID) ([]*entity.Answer, error) {
-	return s.answerRepo.FindByProblemAndUserGroup(probid, userGroupID)
+func (s *AnswerService) FindByProblemAndUserGroup(group *entity.UserGroup, probid uuid.UUID, userGroupID uuid.UUID) ([]*entity.Answer, error) {
+	if !group.IsFullAccess && group.ID != userGroupID {
+		return nil, e.NewForbiddenError("you cannot fetch other group's answers")
+	}
+
+	answers, err := s.answerRepo.FindByProblemAndUserGroup(probid, userGroupID)
+	if err != nil {
+		return nil, err
+	}
+
+	if !group.IsFullAccess {
+		now := time.Now()
+		for _, ans := range answers {
+			if !now.After(ans.CreatedAt.Add(s.answerLimit)) {
+				ans.Point = nil
+			}
+		}
+	}
+
+	return answers, nil
 }
 
 func (s *AnswerService) Update(id uuid.UUID, req *UpdateAnswerRequest) (*entity.Answer, error) {
