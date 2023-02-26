dc4d8f6aa8dbc0f389b4611ff653c5714b497ee6
k-shir0 <kakishiro2190394@gmail.com>
2023-02-27 07:39:35 +0900

    update: create_usergroup を 最新の DB 形式に対応

--- scripts/create_usergroups.py
+++ scripts/create_usergroups.py
@@ -1,5 +1,6 @@
 from ictsc2021 import Rikka
 
+import os
 import json
 import logging
 from http import client
@@ -17,18 +18,22 @@ def main():
 
     setting = load_setting()
 
-    rikka = Rikka(baseurl="https://contest.ictsc.net/api")
+    rikka = Rikka(baseurl=str(os.environ.get("baseurl")))
 
     print(f"\x1b[33m\n*** signin\x1b[0m")
-    rikka.signin("ictsc", "")
+    rikka.signin(os.environ.get("username"), os.environ.get("password"))
 
     for team in setting["teams"]:
         name = team["name"]
         organization = team["organization"]
         invitation_code = team["invitation_code"]
+        bastion_user = team["bastion_user"]
+        bastion_password = team["bastion_password"]
+        bastion_host = team["bastion_host"]
+        bastion_port = team["bastion_port"]
 
         print(f"\x1b[33m\n*** Create user group {name}\x1b[0m")
-        rikka.create_usergroup(name, organization, invitation_code, False)
+        rikka.create_usergroup(name, organization, invitation_code, False, bastion_user, bastion_password, bastion_host, bastion_port)
 
 
 main()
